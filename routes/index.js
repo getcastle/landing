9baae26525f8914bd765b7faf20b4e0f86fff3a1
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-06-28 15:27:29 -0400

    Use factory pattern to build router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const indexRouter = (config) => {
+  const router = express.Router();
 
-module.exports = router;
+  /* GET home page. */
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+  return router;
+}
+
+module.exports = indexRouter;
